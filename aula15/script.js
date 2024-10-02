a63f122ab8e21949f7387367271b5a4d58600c91
Thais Soares <thaisssoaress11@gmail.com>
2024-10-02 13:24:18 -0300

    aula 15

---
+++ aula15/script.js
@@ -0,0 +1,83 @@
+// if else
+// switch case
+
+// console.log
+// alert
+// confirm
+// prompt
+
+// and &&
+// or ||
+// not !
+
+// let diasDaSemana= Number (prompt("Digite um número pro dia da semana"));
+
+// switch(diasDaSemana){
+//     case 1:
+//         alert("Você selecionou domingo")
+//         break;
+//     case 2:
+//         alert("Você selecionou a segunda")
+//         break;
+//     default:
+//         alert("Valor não encontrado")
+// }
+
+
+// let contagem= 0;
+
+
+// while(contagem <= 10){
+//     console.log(contagem)
+//     contagem ++;
+// }
+
+
+// for(let contagem= 0; contagem <= 10; contagem++){
+//     console.log(contagem)
+// }
+
+
+// let velocidade= 0;
+
+// do{
+//     console.log('A velocidade do carro é de', velocidade )
+// }while(velocidade > 0)
+
+// console.log("Valor final", velocidade)
+
+
+// let nome= prompt("Ola, qual é o seu nome?");
+// let cidade="";
+// let contagem= 0;
+
+// let continuar = prompt("Você visitou alguma cidade? sim/não")
+
+// while(continuar == "sim"){
+//     let cidades= prompt("Qual cidade você visitou?");
+//     cidade += '\n' + '-' + cidades + '\n'
+//     contagem ++
+//     continuar = prompt("Visitou mais alguma cidade?sim/não")
+// }
+
+// alert(`O turista ${nome}, visitou ${contagem} cidades e as cidades foram ${cidade} `)
+
+
+// for(let contagem = 0; contagem <=100; contagem++){
+   
+//     if(contagem % 2 === 0){
+//         console.log(`${contagem} é par `)
+//     } else {
+//         console.log(`${contagem} é impar `)
+//     }
+    
+//     if(contagem % 3 === 0 ){
+//          console.log(`${contagem} é multiplo de 3 `)
+//     }
+//      if(contagem % 5 === 0 ){
+//          console.log(`${contagem} é multiplo de 5 `)
+//     }
+//     if(contagem % 7 === 0 ){
+//          console.log(`${contagem} é multiplo de 7 `)
+//     }
+//  }
