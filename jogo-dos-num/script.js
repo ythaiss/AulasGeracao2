6457350c7642b218a495e0341a9eff9e090e35b2
ythaiss <thaisssoaress11@gmai.com>
2024-10-14 11:08:53 -0300

    Teste jogo dos números

---
+++ jogo-dos-num/script.js
@@ -0,0 +1,37 @@
+let numSorteado = 0;
+let chances = 4;
+
+function numAleatorio (){
+    numSorteado = Math.floor(Math.random() * 51);
+
+    console.log(numSorteado);
+}
+
+function numVerificacao (){
+
+    let selection = document.getElementById('selection').value;
+
+    if (chances <= 1) {
+        alert(`Você não tem mais tentativas. O número sorteado era: ${numSorteado}`); 
+        return;
+    }
+
+    if(selection > 50 || selection < 0){
+        alert('Aposta inválida');
+        return;
+    }
+
+    chances--;
+
+    if( selection > numSorteado){
+        alert(`O número sorteado é MENOR. Você tem ${chances} chance(s)`);
+    } 
+    else if( selection < numSorteado){
+        alert(`O número sorteado é MAIOR. Você tem ${chances} chance(s)`);
+    }
+    else{
+        alert(`Você acertou o número sorteado!!`)
+    }
+}
+
+numAleatorio();
